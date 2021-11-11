c5940375d467e1fd9e2117bdfbccbbba4c8d13b6
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-11-11 13:38:15 -0500

    navigating to dynamic routes

--- nextjs-course-routes/pages/clients/index.js
+++ nextjs-course-routes/pages/clients/index.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    {
+      id: "max",
+      name: "Max",
+    },
+    {
+      id: "manu",
+      name: "Manuel",
+    },
+  ];
+
   return (
     <div>
       <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li>
+              <Link href={"clients/" + client.id}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
