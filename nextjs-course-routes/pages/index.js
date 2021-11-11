8f79986a1647cec6209bca49d531f348926a438e
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-11-11 13:18:20 -0500

    navigating with the link component

--- nextjs-course-routes/pages/index.js
+++ nextjs-course-routes/pages/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 function Homepage() {
   return (
     <div>
       <h1>Homepage! Added something!a fasd fasdfasdfasdfsf</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
