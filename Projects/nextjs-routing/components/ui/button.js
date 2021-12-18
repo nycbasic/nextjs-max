854eb8ec473e4a652cb51bbd1a6ace1caf4c87d2
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-12-18 17:01:51 -0500

    added filter form for filtering events programmatically

--- Projects/nextjs-routing/components/ui/button.js
+++ Projects/nextjs-routing/components/ui/button.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import classes from "./css/button.module.css";
 
-const Button = ({ link, children }) => {
-  return (
-    <Link href={link}>
-      <a className={classes.btn}>{children}</a>
-    </Link>
-  );
+const Button = ({ link, children, onClick }) => {
+  if (link) {
+    return (
+      <Link href={link}>
+        <a className={classes.btn}>{children}</a>
+      </Link>
+    );
+  }
+
+  return <button className={classes.btn} onClick={onClick}>{children}</button>
 };
 
 export default Button;
--- Projects/nextjs-routing/pages/events/index.js
+++ Projects/nextjs-routing/pages/events/index.js
@@ -1,10 +1,24 @@
+import { useRouter } from "next/router";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
-function Events() {
+const Events = () => {
+  const events = getAllEvents();
+  const router = useRouter();
+
+  const searchEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
+
   return (
     <div>
-      <h1>All Events!</h1>
+      <EventsSearch onSearch={searchEventsHandler} />
+      <EventList events={events} />
     </div>
   );
-}
+};
 
 export default Events;
