1a670bd4df3f15212984eae05499d8ba9647be59
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-12-26 21:12:38 -0500

    working with getStaticProps

--- Tutorials/nextjs-pre-rend-data-fetch/static-generation/pages/index.js
+++ Tutorials/nextjs-pre-rend-data-fetch/static-generation/pages/index.js
@@ -1,5 +1,21 @@
-function Home (){
-  return <div>Hello World!</div>;
-};
+function Home(props) {
+  const { products } = props;
+  return (
+    <ul>
+      {products.map((val) => {
+        console.log(val.title);
+        <li key={val.id}>{val.title}</li>;
+      })}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }],
+    },
+  };
+}
 
 export default Home;
