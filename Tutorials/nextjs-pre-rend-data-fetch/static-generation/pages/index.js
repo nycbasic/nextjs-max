845fae37fca99fc4518cd9a5f606a881b1a328ec
Spoon <spoon@Spoons-MacBook-Air.local>
2022-01-02 10:28:15 -0500

    using getStaticProps to run serverside code

--- Tutorials/nextjs-pre-rend-data-fetch/static-generation/pages/index.js
+++ Tutorials/nextjs-pre-rend-data-fetch/static-generation/pages/index.js
@@ -1,19 +1,25 @@
+import fs from "fs/promises";
+
+import path from "path";
+
 function Home(props) {
   const { products } = props;
   return (
     <ul>
       {products.map((val) => {
-        console.log(val.title);
-        <li key={val.id}>{val.title}</li>;
+        return <li key={val.id}>{val.title}</li>;
       })}
     </ul>
   );
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
   return {
     props: {
-      products: [{ id: "p1", title: "Product 1" }],
+      products: data.products,
     },
   };
 }
