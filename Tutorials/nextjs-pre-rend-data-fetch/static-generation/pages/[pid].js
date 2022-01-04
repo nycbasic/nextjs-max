909a1d1bb4bfa5810cd4fadc5cb0c0104c7bc25d
Spoon <spoon@Spoons-MacBook-Air.local>
2022-01-04 17:33:28 -0500

    retrieving data

---
+++ Tutorials/nextjs-pre-rend-data-fetch/static-generation/pages/[pid].js
@@ -0,0 +1,10 @@
+function productDetailPage() {
+  return <></>;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid
+}
+
+export default productDetailPage;
