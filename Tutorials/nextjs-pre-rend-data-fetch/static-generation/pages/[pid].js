95de65cec6309a84addc51c7bbf8dd07fce1e242
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-01-08 23:38:39 -0500

    loading paths dynamically

--- Tutorials/nextjs-pre-rend-data-fetch/static-generation/pages/[pid].js
+++ Tutorials/nextjs-pre-rend-data-fetch/static-generation/pages/[pid].js
@@ -16,14 +16,17 @@ function productDetailPage(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+  return data;
+}
 
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+  const data = await getData();
   const product = data.products.find((product) => product.id === productId);
   return {
     props: {
@@ -33,11 +36,12 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  const params = ids.map((id) => ({ params: { pid: id } }));
   return {
-    paths: [
-      { params: { pid: "p1" } }
-    ],
-    fallback: "blocking",
+    paths: params,
+    fallback: false,
   };
 }
 
