e28c188bafb79d12637db5814595d0d81bb0b45e
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-02-22 08:16:31 -0500

    starting to add react context

--- Projects/nextjs-api-routes-prj/pages/_app.js
+++ Projects/nextjs-api-routes-prj/pages/_app.js
@@ -1,17 +1,18 @@
-import Head from 'next/head';
-
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Head from "next/head";
+import Notification from "../components/notifications/notifications";
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name="description" content="NextJS Events" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="test" message="this is a test" status="error" />
     </Layout>
   );
 }
