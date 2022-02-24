a94dd8c50cbc0482dc2d03382e21cf1c710f6c35
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-02-24 07:21:13 -0500

    added styling to the navigation and logo

--- Projects/nextjs-blog-prj/components/layout/logo.js
+++ Projects/nextjs-blog-prj/components/layout/logo.js
@@ -1,5 +1,7 @@
-export default function Logo () {
-    return <div>
-        
-    </div>
+import classes from "./logo.module.css";
+
+const { logo } = classes;
+
+export default function Logo() {
+  return <div className={logo}>Spoon' Next Blog</div>;
 }
--- Projects/nextjs-blog-prj/components/layout/main-navigation.js
+++ Projects/nextjs-blog-prj/components/layout/main-navigation.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import Logo from "./logo";
+import classes from "./main-navigation.module.css";
+
+const {header} = classes;
 
 export default function MainNavigation() {
   return (
-    <header>
+    <header className={header}>
       <Link href="/">
         {/* The link component does not create an anchor tag if the child is not text */}
         <a>
